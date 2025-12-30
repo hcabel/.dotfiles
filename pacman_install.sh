10a425043685bd6a85d1da577c521db8d97de762
hcabel <coding@hugocabel.com>
2025-12-30 21:33:39 +0100

    RESTRUCTURE

--- pacman_install.sh
+++
@@ -1,8 +0,0 @@
-sudo pacman -S --noconfirm \
-    zoxide \ # better cd command
-    eza \ # modern ls replacement
-    lazygit \ # git overpowered TUI
-    nvim \ # Code editor
-    waybar \ # status bar
-
-
