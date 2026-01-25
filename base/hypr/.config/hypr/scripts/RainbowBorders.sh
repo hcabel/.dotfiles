7a93e9bf9aeda8b88ebf3f7e083b90e1ad870280
hcabel <coding@hugocabel.com>
2026-01-25 14:46:14 +0100

    Rainbow border for group too

--- base/hypr/.config/hypr/scripts/RainbowBorders.sh
+++ base/hypr/.config/hypr/scripts/RainbowBorders.sh
@@ -7,5 +7,17 @@ function random_hex() {
     echo $random_hex
 }
 
+hex_1=$(random_hex)
+hex_2=$(random_hex)
+hex_3=$(random_hex)
+hex_4=$(random_hex)
+hex_5=$(random_hex)
+hex_6=$(random_hex)
+hex_7=$(random_hex)
+hex_8=$(random_hex)
+hex_9=$(random_hex)
+hex_10=$(random_hex)
+
 # rainbow colors only for active window
-hyprctl keyword general:col.active_border $(random_hex)  $(random_hex) $(random_hex) $(random_hex) $(random_hex) $(random_hex) $(random_hex) $(random_hex) $(random_hex) $(random_hex)  270deg
+hyprctl keyword general:col.active_border $hex_1 $hex_2 $hex_3 $hex_4 $hex_5 $hex_6 $hex_7 $hex_8 $hex_9 $hex_10 270deg
+hyprctl keyword group:col.border_active $hex_1 $hex_2 $hex_3 $hex_4 $hex_5 $hex_6 $hex_7 $hex_8 $hex_9 $hex_10 270deg
